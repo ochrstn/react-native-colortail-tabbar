a7c4c02e0639d3f041549bf5f397e9069886eb7d
Oliver Christen <christen@app-logik.de>
2019-05-04 14:08:00 +0200

    Extends the example project with a react-navigation integration

--- example/App.tsx
+++ example/App.tsx
@@ -1,63 +1,256 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import ColorTailTabBar, { ColortailTabData } from 'react-native-colortail-tabbar';
+import React, { Component } from 'react'
+import {
+  Image,
+  View,
+  SafeAreaView,
+  Dimensions,
+  ImageSourcePropType,
+} from 'react-native'
+import {
+  createBottomTabNavigator,
+  createAppContainer,
+  BottomTabBarProps,
+} from 'react-navigation'
+import ColorTailTabBar, {
+  ColortailTabData, interpolate
+} from 'react-native-colortail-tabbar'
 
-const SampleIcon = (props: {opacity: number, size: number}) => <View style={{opacity: props.opacity, width: props.size, height: props.size, borderRadius: 7.5, borderWidth: 4, borderColor: '#FFFFFF'}} />;
+import home from './icons/home.png'
+import assessment from './icons/assessment.png'
+import basket from './icons/basket.png'
+import star from './icons/star.png'
+import apps from './icons/apps.png'
 
-const tabs1: ColortailTabData[] = [
+interface AnimatedTabProps {
+  size: number;
+  opacity: number;
+}
+
+const ImageIcon = ({
+  size,
+  opacity,
+  icon
+}: AnimatedTabProps & {icon: ImageSourcePropType}) => {
+
+  return(
+    <Image
+      style={{ width: size, height: size, opacity }}
+      source={icon}
+    />
+  )
+}
+
+const tabs: Array<ColortailTabData<AnimatedTabProps>> = [
   {
     color: '#e7645b',
-    renderTab: (props) => <SampleIcon opacity={props.opacity} size={props.size} />
+    renderAnimatedTab: props => <ImageIcon {...props} icon={home} />,
   },
   {
     color: '#ffbd73',
-    renderTab: (props) => <SampleIcon opacity={props.opacity} size={props.size} />
+    renderAnimatedTab: props => <ImageIcon {...props} icon={assessment} />,
   },
   {
     color: '#73adce',
-    renderTab: (props) => <SampleIcon opacity={props.opacity} size={props.size} />
+    renderAnimatedTab: props => <ImageIcon {...props} icon={basket} />,
   },
   {
     color: '#e76394',
-    renderTab: (props) => <SampleIcon opacity={props.opacity} size={props.size} />
+    renderAnimatedTab: props => <ImageIcon {...props} icon={star} />,
   },
   {
     color: '#73d6b5',
-    renderTab: (props) => <SampleIcon opacity={props.opacity} size={props.size} />
+    renderAnimatedTab: props => <ImageIcon {...props} icon={apps} />,
   }
-];
+]
 
-interface State {
-  activeTab: number;
+class Tab1 extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, padding: 25 }}>
+        <View
+          style={{ flex: 1, backgroundColor: '#5c4f71', borderRadius: 5 }}
+        />
+      </View>
+    )
+  }
 }
 
-export default class App extends Component<{}, State> {
+// tslint:disable-next-line: max-classes-per-file
+class Tab2 extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, padding: 25 }}>
+        {[1, 2, 3, 4, 5].map(x => (
+          <View
+            key={x}
+            style={{ flex: 1, flexDirection: 'row', marginVertical: 5 }}
+          >
+            <View
+              style={{
+                width: 75,
+                height: 75,
+                marginHorizontal: 15,
+                backgroundColor: '#5c4f71',
+                borderRadius: 100,
+              }}
+            />
+            <View
+              style={{
+                flex: 1,
+                height: 75,
+                marginHorizontal: 15,
+                backgroundColor: '#5c4f71',
+                borderRadius: 5,
+              }}
+            />
+          </View>
+        ))}
+      </View>
+    )
+  }
+}
 
-  constructor(props: {}) {
-    super(props);
-    this.state = { activeTab: 0 };
+// tslint:disable-next-line: max-classes-per-file
+class Tab3 extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, padding: 25 }}>
+        {[1, 2, 3, 4, 5].map(x => (
+          <View
+            key={x}
+            style={{
+              flex: 1,
+              marginVertical: 5,
+              backgroundColor: '#5c4f71',
+              borderRadius: 5,
+            }}
+          />
+        ))}
+      </View>
+    )
+  }
+}
+
+// tslint:disable-next-line: max-classes-per-file
+class Tab4 extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, padding: 25 }}>
+        {[1, 2, 3, 4, 5].map(x => (
+          <View key={x} style={{ flex: 1, marginVertical: 5 }}>
+            {[1, 2, 3].map(y => (
+              <View
+                key={y}
+                style={{
+                  width: Math.random() * (Dimensions.get('window').width - 100),
+                  height: 20,
+                  marginVertical: 5,
+                  backgroundColor: '#5c4f71',
+                  borderRadius: 5,
+                }}
+              />
+            ))}
+          </View>
+        ))}
+      </View>
+    )
+  }
+}
+
+// tslint:disable-next-line: max-classes-per-file
+class Tab5 extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, padding: 25 }}>
+        {[1, 2, 3, 4, 5].map(x => (
+          <View
+            key={x}
+            style={{ flex: 1, flexDirection: 'row', marginVertical: 5 }}
+          >
+            {[1, 2, 3].map(y => (
+              <View
+                key={y}
+                style={{
+                  flex: 1,
+                  marginHorizontal: 5,
+                  backgroundColor: '#5c4f71',
+                  borderRadius: 5,
+                }}
+              />
+            ))}
+          </View>
+        ))}
+      </View>
+    )
+  }
+}
+
+// tslint:disable-next-line: max-classes-per-file
+class MyTabBar extends Component<BottomTabBarProps, { activeTab: number }> {
+  state = {
+    activeTab: 0,
+  }
+
+  onTabPress = (activeTab: number) => {
+    this.setState({ activeTab })
+
+    switch (activeTab) {
+      default:
+      case 0:
+        this.props.navigation.navigate('Tab1')
+        break
+      case 1:
+        this.props.navigation.navigate('Tab2')
+        break
+      case 2:
+        this.props.navigation.navigate('Tab3')
+        break
+      case 3:
+        this.props.navigation.navigate('Tab4')
+        break
+      case 4:
+        this.props.navigation.navigate('Tab5')
+        break
+    }
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-        <ColorTailTabBar
-          tabs={tabs1}
-          color="#5c4f71"
-          activeTabIndex={this.state.activeTab}
-          onTabPress={ (activeTab) => { this.setState({activeTab}); } }
-        />
-      </View>
-    );
+      <ColorTailTabBar
+        tabs={tabs}
+        color='#5c4f71'
+        activeTabIndex={this.state.activeTab}
+        onTabPress={this.onTabPress}
+        duration={200}
+        from={{size: 22, opacity: 0.5}}
+        to={{size: 30, opacity: 1.0}}
+      />
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+const TabNavigation = createBottomTabNavigator(
+  {
+    Tab1: { screen: Tab1 },
+    Tab2: { screen: Tab2 },
+    Tab3: { screen: Tab3 },
+    Tab4: { screen: Tab4 },
+    Tab5: { screen: Tab5 },
   },
-});
+  {
+    tabBarComponent: (props: BottomTabBarProps) => <MyTabBar {...props} />,
+  },
+)
+
+const AppContainer = createAppContainer(TabNavigation)
+
+// tslint:disable-next-line: max-classes-per-file
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#423853' }}>
+        <AppContainer />
+      </SafeAreaView>
+    )
+  }
+}
